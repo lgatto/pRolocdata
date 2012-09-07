fcab2df0c8944a0155ee6e70e2d80993af14c742
lgatto <laurent.gatto@gmail.com>
2012-09-07 20:56:31 +0100

    misc minor fixes

--- R/data.R
+++ R/data.R
@@ -1,14 +1,18 @@
 pRolocdata <- function() {
-  data.frame(name = c("dunkley2006",
+  data.frame(name = c(
+               "dunkley2006",
                "foster2006",
                "tan2009r1",
                "tan2009r2",
-               "tan2009r3"),
-             description = c("Dunkley et al. PNAS (2006)",
+               "tan2009r3",
+               "at_chloro"),
+             description = c(
+               "Dunkley et al. PNAS (2006)",
                "Foster et al. Cell (2006)",
                "Tan et al. JPR (2009)",
                "Tan et al. JPR (2009)",
-               "Tan et al. JPR (2009)"))
+               "Tan et al. JPR (2009)",
+               "Ferro et al. MCP (2010)"))
 }
 
 
--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   x <- apply(pRolocdata(), 1, function(.d) paste(.d, collapse = " from "))
   n <- length(x)
   x <- paste0(" ", 1:n, ": ", x, "\n")  
-  msg <- c(paste("\nThis is pRolocdata version",
+  msg <- c(paste0("\nThis is pRolocdata version ",
                  packageVersion("pRolocdata"), ".\n"),
            "Available data sets:\n",
            x)           
