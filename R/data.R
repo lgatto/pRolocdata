dc89c36e1cf0b33ba1236d974d0dc67f624b5dd2
lgatto <laurent.gatto@gmail.com>
2012-09-08 10:42:21 +0100

    pRolocdata uses data and at_chloro example

--- R/data.R
+++ R/data.R
@@ -1,18 +1,4 @@
-pRolocdata <- function() {
-  data.frame(name = c(
-               "dunkley2006",
-               "foster2006",
-               "tan2009r1",
-               "tan2009r2",
-               "tan2009r3",
-               "at_chloro"),
-             description = c(
-               "Dunkley et al. PNAS (2006)",
-               "Foster et al. Cell (2006)",
-               "Tan et al. JPR (2009)",
-               "Tan et al. JPR (2009)",
-               "Tan et al. JPR (2009)",
-               "Ferro et al. MCP (2010)"))
-}
+pRolocdata <- function() 
+  data(package = "pRolocdata")
 
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-  x <- apply(pRolocdata(), 1, function(.d) paste(.d, collapse = " from "))
-  n <- length(x)
-  x <- paste0(" ", 1:n, ": ", x, "\n")  
-  msg <- c(paste0("\nThis is pRolocdata version ",
-                 packageVersion("pRolocdata"), ".\n"),
-           "Available data sets:\n",
-           x)           
+  msg <- paste0("\nThis is pRolocdata version ",
+                packageVersion("pRolocdata"), ".\n",
+                "Use 'pRolocdata()' to list available data sets.")
   packageStartupMessage(msg)  
 }
