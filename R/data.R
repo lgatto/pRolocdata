e704f068aab288f8f7049a867ae4e5afd1a715d0
Laurent <lg390@cam.ac.uk>
2014-08-28 22:17:26 +0100

    added experimentData(nikolovski2014)

--- R/data.R
+++ R/data.R
@@ -1,4 +1,2 @@
 pRolocdata <- function() 
   data(package = "pRolocdata")
-
-
--- inst/scripts/nikolovski2014.R
+++ inst/scripts/nikolovski2014.R
@@ -1,9 +1,6 @@
 library("pRolocdata")
 
-f <- c("../extdata/245589ST2_protein_distributions.csv",
-       "../extdata/245589ST3_MarkerList_250614.csv",
-       "../extdata/245589ST4_SVMLocalisation_280514.csv")
-
+f <- dir("../extdata", full.names = TRUE, pattern = "245589")
 
 ## Expression data
 ## getEcols(f[1], ",")
@@ -17,6 +14,20 @@ pData(nikolovski2014) <-
     data.frame(gradient = substr(sampleNames(nikolovski2014), 1, 1), 
                fraction = as.numeric(sub("[AB]_", "", sampleNames(nikolovski2014))),
                row.names = sampleNames(nikolovski2014))
+
+experimentData(nikolovski2014) <-
+    new("MIAPE",
+        lab = "Cambridge Centre for Proteomics (CCP)",
+        url = "http://proteomics.bio.cam.ac.uk/",
+        name = "Kathryn S. Lilley",
+        email = "k.s.lilley@bioc.cam.ac.uk",
+        samples = list(
+            species = "Arabidopsis thaliana",
+            tissue = ""),
+        title = "Label free protein quantification for plant Golgi protein localisation and abundance",
+        abstract = "The proteomic composition of the Arabidopsis Golgi apparatus is currently reasonably well documented; however little is known about the relative abundances between different proteins within this compartment. Accurate quantitative information of Golgi resident proteins is of great importance: it facilitates a better understanding of the biochemical processes which take place within this organelle, especially those of different polysaccharide synthesis pathways. Golgi resident proteins are challenging to quantify since the abundance of this organelle is relatively low within the cell. In this study an organelle fractionation approach, targeting the Golgi apparatus, was combined with a label free quantitative mass spectrometry (MS), data-independent acquisition (DIA) method employing ion mobility separation known as LC-IMS-MSE (or HDMSE), to simultaneously localize proteins to the Golgi apparatus and assess their relative quantity. In total 102 Golgi localised proteins were quantified. These data provide new insight into Golgi apparatus organization and demonstrate that organelle fractionation in conjunction with label free quantitative MS is a powerful and relatively simple tool to access protein organelle localisation and their relative abundances. The findings presented open a unique view on the organization of the plant Golgi apparatus, leading towards novel hypotheses centered on the biochemical processes of this organelle. he proteomic composition of the Arabidopsis Golgi apparatus is currently reasonably well documented; however little is known about the relative abundances between different proteins within this compartment. Accurate quantitative information of Golgi resident proteins is of great importance: it facilitates a better understanding of the biochemical processes which take place within this organelle, especially those of different polysaccharide synthesis pathways. Golgi resident proteins are challenging to quantify since the abundance of this organelle is relatively low within the cell. In this study an organelle fractionation approach, targeting the Golgi apparatus, was combined with a label free quantitative mass spectrometry (MS), data-independent acquisition (DIA) method employing ion mobility separation known as LC-IMS-MSE (or HDMSE), to simultaneously localize proteins to the Golgi apparatus and assess their relative quantity. In total 102 Golgi localised proteins were quantified. These data provide new insight into Golgi apparatus organization and demonstrate that organelle fractionation in conjunction with label free quantitative MS is a powerful and relatively simple tool to access protein organelle localisation and their relative abundances. The findings presented open a unique view on the organization of the plant Golgi apparatus, leading towards novel hypotheses centered on the biochemical processes of this organelle.",
+        pubMedIds = "25122472")
+
 
 
 ## Adding markers
