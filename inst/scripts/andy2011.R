8832333b72516ed52fe40b968c6d4240cc86252d
Laurent <lg390@cam.ac.uk>
2016-09-23 17:00:02 +0100

    rename andy2011 HEK293T2011 - close issue #18

--- inst/scripts/andy2011.R
+++ inst/scripts/andy2011.R
@@ -6,21 +6,21 @@ library("pRolocdata")
 ## From Andy's protein file "ispy_results_proteins_stringent.csv"
 ## Peptides were merged into proteins by intensity weighted mean
 andy <- read.csv("../extdata/andy2011_ispy_results_proteins_stringent.csv.gz",
-                 row.names=1)
-.exprs <- as.matrix(andy[,5:12])
-.fData <- andy[,c(1:4,14,15,13)]
-.fData <- new("AnnotatedDataFrame",data=data.frame(as.matrix(.fData)))
-.fData@varMetadata[,1] <- c("UniProtKB accession number", 
-                            "Full protein name", 
-                            "Peptides", 
-                            "Peptide spectrum match", 
+                 row.names = 1)
+.exprs <- as.matrix(andy[, 5:12])
+.fData <- andy[, c(1:4, 14, 15, 13)]
+.fData <- new("AnnotatedDataFrame",data = data.frame(as.matrix(.fData)))
+.fData@varMetadata[,1] <- c("UniProtKB accession number",
+                            "Full protein name",
+                            "Peptides",
+                            "Peptide spectrum match",
                             "PhenoDisco output as described in Breckels et al (2013) Journal of Proteomics. Accepted February 2013", 
                             "Updated protein markers (original markers see column 'markers' plus new protein markers found by phenoDisco and verified by UniprotKB/literature as described in Breckels et al)",
                             "Protein markers")
 .pData <- new("AnnotatedDataFrame",
-              data=data.frame(Fraction.information=c(rep("High Density", 7), 
-                                                     "Soluble/cytosolic"), 
-                              row.names=colnames(.exprs)))
+              data=data.frame(Fraction.information = c(rep("High Density", 7),
+                                                       "Soluble/cytosolic"),
+                              row.names = colnames(.exprs)))
 
 .experiment <- new("MIAPE",
                    lab = "Cambridge Centre for Proteomics (CCP)",
@@ -45,29 +45,28 @@ andy <- read.csv("../extdata/andy2011_ispy_results_proteins_stringent.csv.gz",
                    isolationWidth = 0.5,
                    collisionEnergy = "45% CE HCD (two-stepped, 10% width)",
                    other = list(
-                     experimentType = "Standard LOPIT experimental design on HEK293T. Label: iTRAQ 8-plex. Instrument: LTQ Orbitrap Velos. Scan Mode for Identification: MS2-HCD. Scan Mode for Quantitation: MS2-HCD. Scan Setup: Nth order double play, Top 10 HCD. MS1 Scan: FTMS, resolution = 30000, scan range m/z 380 - 1600. MS2 Scan: FTMS, resolution = 7500, scan range m/z 100 - 2000. Precursor Ion Selection Window: 0.5 Da (very stringent). Collision Energy: 45% CE HCD (two-stepped, 10% width)",
-                     searchParameters = "Search Engine: Mascot. Search Database: UniProt Human. Fixed Modifications: iTRAQ 8-plex (N-term), iTRAQ-8plex (K), MMTS (C). Variable Modifications: iTRAQ 8-plex (Y), Oxidation (M). Enzyme: Trypsin. Max. Missed Cleavages: 2. Decoy Type: None (see Percolator parameters below). Peptide Charge: 5+. Peptide Tolerance: +/- 25 ppm. MS/MS Tolerance: +/- 0.2 Da. Instrument: ESI-ORBITRAP-HCD",
-                     postProcessing = "Maximum Parent Proteins: 1. Unquantifiable spectra (E-value for PSM > 0.05, non-unique sequences, very low ion counts, >2 zero value reporter ions ) removed. Spectra filtered based on several criteria (precursor relative signal, position of switch relative to peak apex, reporter ion intensity) to pick a single peptidotypic spectrum per peptide. Peptides were merged into proteins by intensity weighted mean",
+                       experimentType = "Standard LOPIT experimental design on HEK293T. Label: iTRAQ 8-plex. Instrument: LTQ Orbitrap Velos. Scan Mode for Identification: MS2-HCD. Scan Mode for Quantitation: MS2-HCD. Scan Setup: Nth order double play, Top 10 HCD. MS1 Scan: FTMS, resolution = 30000, scan range m/z 380 - 1600. MS2 Scan: FTMS, resolution = 7500, scan range m/z 100 - 2000. Precursor Ion Selection Window: 0.5 Da (very stringent). Collision Energy: 45% CE HCD (two-stepped, 10% width)",
+                       searchParameters = "Search Engine: Mascot. Search Database: UniProt Human. Fixed Modifications: iTRAQ 8-plex (N-term), iTRAQ-8plex (K), MMTS (C). Variable Modifications: iTRAQ 8-plex (Y), Oxidation (M). Enzyme: Trypsin. Max. Missed Cleavages: 2. Decoy Type: None (see Percolator parameters below). Peptide Charge: 5+. Peptide Tolerance: +/- 25 ppm. MS/MS Tolerance: +/- 0.2 Da. Instrument: ESI-ORBITRAP-HCD",
+                       postProcessing = "Maximum Parent Proteins: 1. Unquantifiable spectra (E-value for PSM > 0.05, non-unique sequences, very low ion counts, >2 zero value reporter ions ) removed. Spectra filtered based on several criteria (precursor relative signal, position of switch relative to peak apex, reporter ion intensity) to pick a single peptidotypic spectrum per peptide. Peptides were merged into proteins by intensity weighted mean",
                        MS = "iTRAQ8",
                        spatexp = "LOPIT",
                        markers.fcol = "pd.markers",
                        prediction.fcol = "pd.2013"
-                     ),
-                   dateStamp = "2011-07"
-                   )               
+                   ),
+                   dateStamp = "2011-07")
 
 .process <- new("MSnProcess",
-                processing=c(
-                  paste("Loaded on ",date(),".",sep=""),
-                  paste("Normalised to sum of intensities.")),
-                 normalised=TRUE,
-                files="andy2011_ispy_results_proteins_stringent.csv")
+                processing = c(
+                    paste("Loaded on ", date(), ".",sep = ""),
+                    paste("Normalised to sum of intensities.")),
+                normalised = TRUE,
+                files = "andy2011_ispy_results_proteins_stringent.csv")
 
 andy2011 <- new("MSnSet",
-                   exprs = .exprs,
-                   phenoData = .pData,
-                   experimentData = .experiment,
-                   featureData = .fData)
+                exprs = .exprs,
+                phenoData = .pData,
+                experimentData = .experiment,
+                featureData = .fData)
 andy2011@processingData <- .process
 
 ## Add updated markers
@@ -94,7 +93,11 @@ stopifnot(pRolocdata:::valid.pRolocmetadata(pRolocmetadata(andy2011)))
 fData(andy2011)$UniProtKB.entry.name <- featureNames(andy2011)
 featureNames(andy2011) <- fData(andy2011)$Accession.No.
 
-if (validObject(andy2011))
-  save(andy2011,file="../../data/andy2011.RData",
-       compress = "xz", compression_level = 9)
-
+if (validObject(andy2011)) {
+    save(andy2011, file = "../../data/andy2011.RData",
+         compress = "xz", compression_level = 9)
+    ## rename HEK293T2011 (new in version 1.11.9)
+    HEK293T2011 <- andy2011
+    save(HEK293T2011, file = "../../data/HEK293T2011.RData",
+         compress = "xz", compression_level = 9)
+}
